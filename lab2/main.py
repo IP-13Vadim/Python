81897ad3e2b2a8797b6e6814cb3790e9319e2b33
IP-13Vadim <90095400+IP-13Vadim@users.noreply.github.com>
2021-10-19 16:47:23 +0300

    lab2

--- lab2/main.py
+++ lab2/main.py
@@ -1,2 +1,15 @@
-print("Hello")
-print(123)
+# 19. Задані дійсні числа x, y. Визначити, чи належить точка з координатами (x, y)
+# заштрихованій частині площини.
+from math import fabs
+
+r = 3
+x = float(input("Enter x coordinate:"))
+y = float(input("Enter y coordinate:"))
+
+result1 = "The point belongs to the shaded part of the plane"
+result2 = "The point does not belongs to the shaded part of the plane"
+
+if (x ** 2) + (y ** 2) >= (r ** 2) and fabs(x) < 3 and fabs(y) < 3 and x>0:
+    print(result1)
+elif x < 0 and y > 0:
+    print(result2)
