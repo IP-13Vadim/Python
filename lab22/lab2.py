7b4021268783e3b7e52bc5dfec52f57fa9a0d5af
IP-13Vadim <90095400+IP-13Vadim@users.noreply.github.com>
2021-10-20 16:44:07 +0300

    add2lab

--- lab22/lab2.py
+++ lab22/lab2.py
@@ -9,7 +9,7 @@ y = float(input("Enter y coordinate:"))
 result1 = "The point belongs to the shaded part of the plane"
 result2 = "The point does not belongs to the shaded part of the plane"
 
-if (x ** 2) + (y ** 2) >= (r ** 2) and fabs(x) < 3 and fabs(y) < 3 and x>0:
+if (x ** 2) + (y ** 2) >= (r ** 2) and fabs(x) < 3 and fabs(y) < 3 and x > 0:
     print(result1)
 elif x < 0 and y > 0:
     print(result2)
